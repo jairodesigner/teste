54ed20db7b80f09fa9d5d75aa3bceabca3d5a128
jairodesigner <noreply>
2015-10-31 20:09:20 +0000

    Add component teste

---
+++ ui/teste.reel/teste.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/teste.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Teste
+ * @extends Component
+ */
+exports.Teste = Component.specialize(/** @lends Teste# */ {
+    constructor: {
+        value: function Teste() {
+            this.super();
+        }
+    }
+});
